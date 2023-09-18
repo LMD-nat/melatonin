7140d24b0326d6a5bea12daa7dfcb43a4a688391
Nathalie Germain <71288067+LMD-nat@users.noreply.github.com>
2023-09-18 13:14:21 -0400

    Update RoB_Vis.R

--- RiskOfBias/RoB_Vis.R
+++ RiskOfBias/RoB_Vis.R
@@ -1,13 +1,14 @@
 # ROB tool for melatonin meta-analysis
 # install.packages("robvis")
 library(devtools)
-devtools::install_github("mcguinlu/robvis")
+# devtools::install_github("mcguinlu/robvis")
 library(robvis)
 library(dplyr)
-
-# import RoB data from Covidence
 library(readr)
-RoB <- read_excel("RoB.csv")
+
+# get data
+urlfile = "https://raw.githubusercontent.com/LMD-nat/melatonin/main/RiskOfBias/RoB.csv"
+RoB <- read_csv(url(urlfile))
 
 # summary plot
 rob_summary(data = RoB, tool = "ROB2")
